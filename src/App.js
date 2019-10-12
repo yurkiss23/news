73f6e8a774ab5c09ecc9da86a01b1530660b4f86
Anonymous <user@rivne.itstep.org>
2019-10-12 10:33:44 +0300

    begin

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+import HomePage from './components/Home';
+import LoginPage from './components/Auth/Login';
+import Header from './components/header';
+
+function App() {
+  return (
+    <Router>
+      
+      <Header/>
+
+      <Switch>
+        <Route exact path="/">
+          <HomePage/>
+        </Route>
+        <Route exact path="/login">
+          <LoginPage/>
+        </Route>
+
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/components/header.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import {
+    Link
+  } from "react-router-dom";
+
+class Header extends Component {
+    state = {  }
+    render() { 
+        return (
+            <header>
+                <ul className="nav">
+                    <li className="nav-item">
+                        <Link className="nav-link" to="/">Home</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link className="nav-link" to="/login">Login</Link>
+                    </li>
+                </ul>
+            </header>
+        );
+    }
+}
+ 
+export default Header;
